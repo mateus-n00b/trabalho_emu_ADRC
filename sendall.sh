2b3c8654d726ecc55e240b8f4f92a45d9dcd5dd3
mateus <mateus.aluufc@gmail.com>
2016-12-13 13:29:05 -0300

    Delete sendall.sh

--- sendall.sh
+++
@@ -1,6 +0,0 @@
-#[ $# -eq 0 ] && exit
-for x in {1..33}
-do
-     cp *.sh "/tmp/$(ls /tmp/ | grep py)/n${x}.conf" &> /dev/null
-#    [ $? -eq 0 ] || echo "[-] Erro ao copiar para /tmp/pycore.36585/n${x}.conf"
-done
