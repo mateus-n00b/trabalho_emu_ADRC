c4afb9935d83793d3142bd856accea59e296fa97
mateus <mateus.aluufc@gmail.com>
2016-12-13 13:29:46 -0300

    Delete executioner.sh

--- executioner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-[ $# -eq 0 ] && echo "Isira a pasta e o IP do server" && exit
-# for x in {4..33}
-for x in {4..33}
-do
-	vcmd -c  "/tmp/$(ls /tmp/ | grep py)/n${x}" -- bash bot.sh "$1"  &
-	[ $? -eq 0 ] && echo "[+] Comando executado no host $x!" || echo "[-] Erro!"
-done
